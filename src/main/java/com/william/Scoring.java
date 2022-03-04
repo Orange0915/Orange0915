4f0c89c588567106f3fa8ebc45246911a5bac027
William Liu <liu910915@gmal.com>
2022-03-04 12:57:47 +0800

    5th commit

---
+++ src/main/java/com/william/Scoring.java
@@ -0,0 +1,10 @@
+package com.william;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student Jhin = new Student();
+        Jhin.name = "Jhin";
+        Jhin.chinese = 80;
+        Jhin.science = 75;
+    }
+}
