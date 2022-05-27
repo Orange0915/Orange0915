eb1ca542366d6f03c3bfa202e00f4194e473142b
Orange0915 <liu910915@gmail.com>
2022-05-27 12:53:35 +0800

    HW 2

--- src/main/java/com/william/ui/GuessFrame.java
+++ src/main/java/com/william/ui/GuessFrame.java
@@ -6,25 +6,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GuessFrame extends JFrame {
+    JButton button = new JButton("print");
+    JLabel label = new JLabel("number");
+    JTextField number  = new JTextField(10);
     public GuessFrame(){
         super();
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
-        JButton button = new JButton("another");
-        JLabel label = new JLabel("hello3");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed (ActionEvent e){
-                System.out.println("hello2");
-                label.setText("ni hao");
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
             }
         });
 
         setLayout(new FlowLayout());
         add(label);
         add(button);
+        add(number);
         setVisible(true);
     }
     public static void main(String[] args) {
