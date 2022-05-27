c1d200c42a8b33f4cb822b3719441a34f56ba2de
Orange0915 <liu910915@gmail.com>
2022-05-27 23:36:06 +0800

    猜數字 0527

--- src/main/java/com/william/ui/GuessFrame.java
+++ src/main/java/com/william/ui/GuessFrame.java
@@ -1,5 +1,6 @@
 package com.william.ui;
 
+import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -11,6 +12,9 @@ public class GuessFrame extends JFrame {
     JTextField number  = new JTextField(10);
     public GuessFrame(){
         super();
+        Random random = new Random();
+        int aws = random.nextInt(10)+1;
+        System.out.println(aws);
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -18,8 +22,15 @@ public class GuessFrame extends JFrame {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed (ActionEvent e){
-                int num = Integer.parseInt(number.getText());
-                System.out.println(num);
+                    int num = Integer.parseInt(number.getText());
+                    System.out.println(num);
+                    if (num < aws) {
+                        System.out.println("Bigger:");
+                    } else if (num > aws) {
+                        System.out.println("Smaller:");
+                    } else {
+                        System.out.println("Bingo");
+                    }
             }
         });
 
